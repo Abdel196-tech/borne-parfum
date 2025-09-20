6b8288bc76d5c80949be2009cc706d89e036c6c7
Abdel <abdessamade123messaoudi@gmail.com>
2025-09-21 00:48:05 +0200

    version 1 welcome page

--- front/borne-parfums-app/src/app/app.component.ts
+++ front/borne-parfums-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { MenuComponent } from './menu/menu.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,WelcomeComponent,MenuComponent],
+  imports: [RouterOutlet,WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
