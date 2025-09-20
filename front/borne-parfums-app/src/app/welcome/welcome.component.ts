315b1df905c0087fb29a4e1af88a453209fe00b0
haninoualid <haninoualid@gmail.com>
2025-09-20 12:30:29 +0200

    first try of the welcome page

--- front/borne-parfums-app/src/app/welcome/welcome.component.ts
+++ front/borne-parfums-app/src/app/welcome/welcome.component.ts
@@ -1,11 +1,25 @@
+// welcome.component.ts
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.css'
+  styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  lang: 'fr' | 'en' | 'nl' = 'fr';
 
+  constructor(private router: Router) {}
+
+  setLang(l: 'fr'|'en'|'nl') {
+    this.lang = l;
+    // tu peux ajouter i18n logic ici (ex: translate service)
+    // exemple: this.translateService.use(l);
+  }
+
+  onStart() {
+    // Navigue vers le flow principal ; adapte la route à ton app
+    this.router.navigate(['/experience'], { queryParams: { lang: this.lang } });
+  }
 }
