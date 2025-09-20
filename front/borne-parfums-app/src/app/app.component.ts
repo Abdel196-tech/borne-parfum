94e80ba7f5d427b2fbfe05601aefadcefb1cb980
haninoualid <haninoualid@gmail.com>
2025-09-20 12:31:38 +0200

    modifing gitignore and app component

--- front/borne-parfums-app/src/app/app.component.ts
+++ front/borne-parfums-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WelcomeComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/borne-parfums-app/src/app/app.module.ts
+++ front/borne-parfums-app/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WelcomeComponent,
+    MenuComponent
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
